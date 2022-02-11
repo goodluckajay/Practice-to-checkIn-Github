ac8467cc2f930e2484a0b197a3de85882578d88c
goodl <goodluckajay@gmail.com>
2022-02-11 13:21:29 +0530

    First commit

---
+++ src/com/self/Main.java
@@ -0,0 +1,8 @@
+package com.self;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Practice to check in the code into GitHub from Intellij Idea");
+    }
+}
